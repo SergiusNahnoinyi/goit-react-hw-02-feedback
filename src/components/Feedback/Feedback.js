a78b993b53449f800a4eba8081d295a119154d9b
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-09 16:09:54 +0200

    Добавляет начальную разметку

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -6,6 +6,26 @@ export default class Feedback extends Component {
   static propTypes = {};
 
   render() {
-    return <div className="Feedback">Class Component</div>;
+    return (
+      <section className="feedback">
+        <div className="service-assessment">
+          <h2 className="title">Please leave feedback</h2>
+
+          <button type="button">Good</button>
+          <button type="button">Neutral</button>
+          <button type="button">Bad</button>
+        </div>
+
+        <div className="statistics">
+          <h2 className="title">Statistics</h2>
+
+          <ul className="list">
+            <li className="item">Good</li>
+            <li className="item">Neutral</li>
+            <li className="item">Bad</li>
+          </ul>
+        </div>
+      </section>
+    );
   }
 }
