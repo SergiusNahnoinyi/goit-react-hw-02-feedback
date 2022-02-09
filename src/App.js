552f90bee119de8ec7864d2ae258b6df1492ee2c
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-09 15:43:52 +0200

    Создает компонент Feedback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Feedback from './components/Feedback';
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Feedback />
+    </div>
+  );
 }
