46db6df661513b86bf331be614856be9cda1e907
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-09 15:48:03 +0200

    Создает класс Feedback

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,3 +1,11 @@
-export default function Feedback() {
-  return <div className="Feedback"></div>;
+import React, { Component } from 'react';
+
+export default class Feedback extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return <div className="Feedback">Class Component</div>;
+  }
 }
