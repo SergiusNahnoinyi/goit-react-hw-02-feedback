ef899331bd7c60e18a1da51004cc5fcc7449d6d2
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-13 17:03:12 +0200

    Стилизирует секцию и контейнер с контентом

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import s from './Section.module.css';
 
 export default function Section({ title, children }) {
   return (
-    <section className={title}>
-      <h2 className="title">{title}</h2>
+    <section className={s.section}>
+      <h2 className={s.title}>{title}</h2>
       {children}
     </section>
   );
